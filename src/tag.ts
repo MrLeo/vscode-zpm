2ed78c55df56f6ed3588798a3305a4e82b58ee8e
Leo <leo@xuebin.me>
2019-03-18 20:54:50 +0800

    createStatusBarItem tag

--- src/tag.ts
+++ src/tag.ts
@@ -5,7 +5,7 @@
  * @version: 0.0.0
  * @Description: 🔖 创建Tag
  * @Date: 2019-03-13 16:04:30
- * @LastEditTime: 2019-03-18 20:49:46
+ * @LastEditTime: 2019-03-18 20:54:36
  */
 
 import { commands, Disposable, window, ProgressLocation, StatusBarAlignment } from 'vscode'
@@ -69,7 +69,7 @@ export class Tag {
   constructor() {
     let tagBtn = window.createStatusBarItem(StatusBarAlignment.Left, 4.5)
     tagBtn.command = Commands.tag
-    tagBtn.text = `tag`
+    tagBtn.text = `$(tag)`
     tagBtn.tooltip = '创建tag'
     tagBtn.show()
 
